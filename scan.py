3dbddbedeb419db584445a2467c3a268aad5a435
Romain Crestey <rcrestey@gmail.com>
2014-10-17 13:48:31 +0200

    contour + rgb fix

--- scan.py
+++ scan.py
@@ -22,26 +22,31 @@ args = vars(ap.parse_args())
 # load the image and compute the ratio of the old height
 # to the new height, clone it, and resize it
 image = cv2.imread(args["image"])
+
+b,g,r = cv2.split(image)       # get b,g,r
+image = cv2.merge([r,g,b])     # switch it to rgb
+
 ratio = image.shape[0] / 500.0
 orig = image.copy()
 image = imutils.resize(image, height = 500)
 
 # convert the image to grayscale, blur it, and find edges
 # in the image
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-gray = cv2.GaussianBlur(gray, (5, 5), 0)
-edged = cv2.Canny(gray, 75, 200)
+# gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# gray = cv2.GaussianBlur(gray, (5, 5), 0)
+edged = cv2.Canny(image, 75, 200)
 
 # find the contours in the edged image, keeping only the
 # largest ones, and initialize the screen contour
 (cnts, _) = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:1]
+cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:5]
 
 # loop over the contours
 for c in cnts:
 	# approximate the contour
 	peri = cv2.arcLength(c, True)
 	approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+	print(cv2.contourArea(c))
 
 	# if our approximated contour has four points, then we
 	# can assume that we have found our screen
@@ -74,3 +79,4 @@ fig.add_axes(ax)
 ax.imshow(final, aspect='normal', cmap = cm.gray)
 
 plt.savefig("out.pdf", format='pdf')
+# plt.show()
